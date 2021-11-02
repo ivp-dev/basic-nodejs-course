4c591ff1684bb0623b78c2b6eda63d00ed7c3099
ivp.net <ivp.net@outlook.com>
2021-11-02 15:36:06 +0300

    refactoring

--- my_caesar_cli.js
+++ my_caesar_cli.js
@@ -2,16 +2,22 @@ const process = require('process');
 
 const argv = process.argv.slice(2);
 
-const available_commands = [
+//available cli commands  
+const acmd = [
   ['-c', '--config', true  /*required*/], // config index:0
   ['-i', '--input', false  /*optional*/], // input  index:1
   ['-o', '--output', false /*optional*/]  // output index:2
 ];
 
-const validate_argv = (argv) => {
-  for (let idx = 0; idx < available_commands.length; idx++) {
-    const cmd = available_commands[idx];
-    const input_cmds = argv.reduce((acc, arg) => {
+/**
+ * Validate input commands
+ * @param {*} argv 
+ * @param {*} acmd 
+ */
+const validate_argv = (argv, acmd) => {
+  for (let idx = 0; idx < acmd.length; idx++) {
+    const cmd = acmd[idx];
+    const input_cmd = argv.reduce((acc, arg) => {
 
       if (arg === cmd[0] || arg === cmd[1]) {
         acc.push(arg)
@@ -20,33 +26,36 @@ const validate_argv = (argv) => {
       return acc;
     }, [])
 
-    if (input_cmds.length > 1) {
+    if (input_cmd.length > 1) {
       throw new Error(`Parameter must be set once. Wrong parameter: ${cmd[0]} (${cmd[1]})`);
-    } else if (!input_cmds.length && cmd[2]) {
+    } else if (!input_cmd.length && cmd[2]) {
       throw new Error(`Parameter ${cmd[0]} or ${cmd[1]} is required`);
     }
   }
 }
 
-const get_arg_value = (argv, acmv, idx) => {
-  let index = argv.indexOf(acmv[idx][0]);
+/**
+ * Get argv value
+ * @param {*} argv 
+ * @param  {...any} acmd 
+ * @returns 
+ */
+const get_arg_value = (argv, ...acmd) => {
+  let index = -1;
 
-  if (index === -1) {
-    index = argv.indexOf(acmv[idx][1]);
-  }
+  for (let idx = 0; idx < acmd.length; idx++) {
+    const cmd = acmd[idx];
+    index = argv.indexOf(cmd);
 
-  if (index > -1 && index + 1 <= argv.length) {
-    return argv[index + 1];
+    if (index !== -1) {
+      return argv[index + 1];
+    }
   }
 }
 
-validate_argv(argv);
+validate_argv(argv, acmd);
 
-const config = argv.reduce((acc, arg) => {
-  return acc;
-}, {
-  'nodePath': process.argv[0],
-  'appPath': process.argv[1]
-})
-
-process.stderr.write(get_arg_value(argv, available_commands, 2));
+const arg_value = get_arg_value(argv, ...acmd[0]);
+if (typeof arg_value !== 'undefined') {
+  process.stderr.write(arg_value);
+}
